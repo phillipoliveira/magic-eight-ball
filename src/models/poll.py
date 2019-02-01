b109e2b9c6c0aec26e59041e8926ef321db7c7a0
Phillip Oliveira <phillip.j.oliveira@gmail.com>
2019-01-31 21:29:52 -0500

    bug

--- src/models/poll.py
+++ src/models/poll.py
@@ -43,7 +43,7 @@ class Poll(object):
                             "value": "{}".format(cls.emoji_dict(count))})
         print(formated_options)
         poll_head = {
-                    "text": question,
+                    "text": str(question),
                     "response_type": "in_channel",
                     "attachments": [
                                         {
