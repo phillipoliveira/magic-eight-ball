746196291e08b7a6fb6244c989f1bd11169147a1
Phillip Oliveira <phillip.j.oliveira@gmail.com>
2019-02-04 10:53:16 -0500

    bug fix

--- src/models/poll.py
+++ src/models/poll.py
@@ -28,27 +28,24 @@ class Poll(object):
     @classmethod
     def create_poll(cls, poll):
         question = poll[0]
-        print(question)
         options = poll[1:]
-        print(options)
-        formated_options = ""
+        formatted_options = ""
         actions = list()
         count = 0
         for option in options:
             count += 1
             option_stg = "{} {}\n".format(cls.emoji_dict(count), (options[count - 1])[0])
-            formated_options = formated_options + option_stg
+            formatted_options = formatted_options + option_stg
             actions.append({"name": "game",
                             "text": "{}".format(cls.emoji_dict(count)),
                             "type": "button",
                             "value": "{}".format(cls.emoji_dict(count))})
-        print(formated_options)
         poll_head = {
                     "text": question[0],
                     "response_type": "in_channel",
                     "attachments": [
                                         {
-                                            "text": formated_options,
+                                            "text": formatted_options,
                                             "fallback": "Something went wrong!",
                                             "callback_id": "poll",
                                             "color": "#3AA3E3",
@@ -64,6 +61,7 @@ class Poll(object):
     def form_response(user, action_value, question, attachments):
         for attachment in attachments:
             if "actions" in attachment:
+                formatted_options = attachment["text"]
                 for action in attachment["actions"]:
                     action["text"] = action["text"].replace(user, "")
                     if action["value"] == action_value:
@@ -71,11 +69,17 @@ class Poll(object):
                             action["text"] = action["text"] + " @" + user
                         else:
                             action["text"] = action["text"] + "\n@" + user
+                attachments = {"text": formatted_options,
+                               "fallback": "Something went wrong!",
+                               "callback_id": "poll",
+                               "color": "#3AA3E3",
+                               "attachment_type": "default",
+                               "actions": attachment["actions"]}
                 response = json.dumps({"text": question,
-                            "response_type": "in_channel",
-                            "attachments": attachments
+                                       "response_type": "in_channel",
+                                       "attachments": attachments
                             })
-        return response
+                return response
 
 
 
