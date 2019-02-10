c20b359c8127639ad990d9092389de044325d434
Phillip Oliveira <phillip.j.oliveira@gmail.com>
2019-02-10 13:55:44 -0500

    bug

--- src/models/poll.py
+++ src/models/poll.py
@@ -44,7 +44,7 @@ class Poll(object):
         count = 0
         for option in options:
             count += 1
-            option_stg = "{} {}\n".format(self.emoji_dict[count], (options[count - 1].rstrip())
+            option_stg = "{} {}\n".format(self.emoji_dict[count], options[count - 1].rstrip())
             formatted_options = formatted_options + option_stg
             actions.append({"name": "game",
                             "text": "{}".format(self.emoji_dict[count]),
